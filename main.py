98ab9e465cc7341c6ef359e12f28929ff7f188de
kallerichter <kalle.richter@online.de>
2026-01-22 19:29:33 +0100

    neuer testshit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("was geht yallah")
+wie_gehts = input("Wie gehts ")
+print("mir gehts auch " + wie_gehts)
+name = input("wie heißt du ")
+print("hallo " + name)
+if name == ("Jonah"):
+    print("mein code ist schon krass")
+else:
+    print("Hey Boss")

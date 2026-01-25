e66c3828636e773c477babb5ec56a5099f165645
kallerichter <kalle.richter@online.de>
2026-01-25 16:05:52 +0100

    GANZ VIEL SHIT

--- client.py
+++ client.py
@@ -3,6 +3,18 @@ import socket
 s = socket.socket()
 host = "localhost"
 port = 50007
+client_text = '''
+,,    ,,                               
+  .g8"""bgd `7MM    db                         mm    
+.dP'     `M   MM                               MM    
+dM'       `   MM  `7MM   .gP"Ya  `7MMpMMMb.  mmMMmm  
+MM            MM    MM  ,M'   Yb   MM    MM    MM    
+MM.           MM    MM  8M""""""   MM    MM    MM    
+`Mb.     ,'   MM    MM  YM.    ,   MM    MM    MM    
+  `"bmmmd'  .JMML..JMML. `Mbmmd' .JMML  JMML.  `Mbmo
+'''
+
+print(client_text)
 username = input("enter username: ")
 
 print("connecting")
--- server.py
+++ server.py
@@ -2,11 +2,21 @@ import socket
 
 HOST = ''
 PORT = 50007
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
+server_text = '''
 
+ .M"""bgd                                                
+,MI    "Y                                                
+`MMb.      .gP"Ya  `7Mb,od8 `7M'   `MF' .gP"Ya  `7Mb,od8 
+  `YMMNq. ,M'   Yb   MM' "'   VA   ,V  ,M'   Yb   MM' "' 
+.     `MM 8M""""""   MM        VA ,V   8M""""""   MM     
+Mb     dM YM.    ,   MM         VVV    YM.    ,   MM     
+P"Ybmmd"   `Mbmmd' .JMML.        W      `Mbmmd' .JMML.
+'''
+
+print(server_text)
 print("Server listening...")
 conn, addr = s.accept()
 print('Connected by', addr)
